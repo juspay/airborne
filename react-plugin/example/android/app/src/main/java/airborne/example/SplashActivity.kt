85492e9f14de9254ff1edabaff3acc473e78fe3b
george.james <george.james@juspay.in>
2025-07-05 13:35:24 +0530

    fix: Change android app to boot on boot complete

---
+++ react-plugin/example/android/app/src/main/java/airborne/example/SplashActivity.kt
@@ -0,0 +1,30 @@
+package airborne.example
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    var hasBootCompleted = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        if (applicationContext is MainApplication) {
+            (applicationContext as MainApplication).bootCompleteListener = {
+                startMainActivity()
+            }
+            if ((applicationContext as MainApplication).isBootComplete) {
+                startMainActivity()
+            }
+        }
+
+    }
+
+    private fun startMainActivity() {
+        if (hasBootCompleted) return
+        hasBootCompleted = true
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
