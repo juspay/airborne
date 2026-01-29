49e17e78b4096485c1b3a7c5e281e06cf6d3e3b4
Yash Rajput <yash.rajput.001@juspay.in>
2026-01-29 12:07:23 +0530

    fix: refactor release creation, edit and clone mode

---
+++ airborne_dashboard/components/release/index.ts
@@ -0,0 +1,12 @@
+export { ReleaseFormProvider, useReleaseForm } from "./ReleaseFormContext";
+export { ReleaseBuilder } from "./ReleaseBuilder";
+export { StepIndicator } from "./StepIndicator";
+export { PaginationControls } from "./PaginationControls";
+export { ReleaseConfirmationDialog } from "./ReleaseConfirmationDialog";
+
+export { ConfigurationStep } from "./steps/ConfigurationStep";
+export { TargetingStep } from "./steps/TargetingStep";
+export { RemoteConfigStep } from "./steps/RemoteConfigStep";
+export { PackageSelectionStep } from "./steps/PackageSelectionStep";
+export { FilePrioritiesStep } from "./steps/FilePrioritiesStep";
+export { ResourcesStep } from "./steps/ResourcesStep";
