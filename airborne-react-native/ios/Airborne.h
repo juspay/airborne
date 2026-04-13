4d85de97eca94d3c634954fbe1bb23a68822a5c9
yuvrajjsingh0 <yuvraj.singh@juspay.in>
2026-04-13 16:44:28 +0530

    feat: added shouldUpdate and checkForUpdate

--- airborne-react-native/ios/Airborne.h
+++ airborne-react-native/ios/Airborne.h
@@ -8,10 +8,12 @@ NS_ASSUME_NONNULL_BEGIN
 + (instancetype)sharedInstanceWithNamespace:(NSString *)aNamespace;
 
 - (instancetype)initWithReleaseConfigURL:(NSString *)releaseConfigURL delegate:(id<AirborneDelegate>)delegate;
+- (instancetype)initWithReleaseConfigURL:(NSString *)releaseConfigURL delegate:(id<AirborneDelegate>)delegate shouldUpdate:(BOOL)shouldUpdate;
 
 - (NSString *)getBundlePath;
 - (NSString *)getFileContent:(NSString *)filePath;
 - (NSString *)getReleaseConfig;
+- (void)checkForUpdate:(void (^)(NSString * _Nonnull))completion;
 
 @end
 
